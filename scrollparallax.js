ebaf539a07603299db0458604b9c41af47356c77
SpiritAxolotl <dgnearman@gmail.com>
2022-02-14 23:24:21 -0700

    tried adding parallax (epic fail)

---
+++ scrollparallax.js
@@ -0,0 +1,22 @@
+alert("Proof that the javascript linked properly");
+
+var body = document.querySelector("body");
+var bg = document.querySelector("#bg");
+
+function setTranslate(xPos, yPos, el) {
+    el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+}
+
+window.addEventListener("DOMContentLoaded", scrollLoop, false);
+
+var xScrollPosition;
+var yScrollPosition;
+
+function scrollLoop() {
+    xScrollPosition = window.scrollX;
+    yScrollPosition = window.scrollY;
+
+    setTranslate(0, yScrollPosition * 1, bg);
+
+    requestAnimationFrame(scrollLoop);
+}
