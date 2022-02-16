b048beda3eb12af6977b8c7c26cb3c6e3e881211
Spax <imstuff@spax.zone>
2022-02-16 08:01:11 -0700

    FUCKING FINALLY FIXED THE PARALLAX BG EFFECT TYSM SORA WOOOOO

--- scrollparallax.js
+++ scrollparallax.js
@@ -1,20 +1,6 @@
-var body = document.querySelector("body");
 var bg = document.querySelector("#bg");
 
-function setTranslate(xPos, yPos, el) {
-    el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
-}
+document.body.onscroll = function() {
 
-window.addEventListener("DOMContentLoaded", scrollLoop, false);
-
-var xScrollPosition;
-var yScrollPosition;
-
-function scrollLoop() {
-    xScrollPosition = window.scrollX;
-    yScrollPosition = window.scrollY;
-
-    setTranslate(0, yScrollPosition * -0.25, bg);
-
-    requestAnimationFrame(scrollLoop);
+document.getElementById('bg').style.backgroundPositionY = (window.scrollY / 2) + "px"
 }
