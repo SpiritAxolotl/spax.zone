c135ce61aa1529d4427163dd877a7ea2750b1c5e
SpiritAxolotl <dgnearman@gmail.com>
2022-02-17 10:36:17 -0700

    fixed timezone now it works properly through all timezones and shit

--- timezonething.js
+++ timezonething.js
@@ -23,15 +23,15 @@ function toTimezone(person){
     if (person[1]=="0"||person[1]=="1"||person[1]=="2"||person[1]=="3"||person[1]=="4"||person[1]=="5"||person[1]=="6"||person[1]=="7"||person[1]=="8"||person[1]=="9"){
         person[1]="0"+person[1]
     }
-    if ((person[0]<0)||(person[0]>23)){
-        person[0]=Math.abs(23-person[0])+1;
-        if ((person[0])>13){
-            person[2]=(person[0]-12)+":"+person[1]+" PM";
-        } else {
-            person[2]=person[0]+":"+person[1]+" AM";
-        }
+    if ((person[0]<1)||(person[0]>24)){
+        person[0]=Math.abs(24-person[0])+1;
+    }
+    if (person[0]==12){
+        person[2]=person[0]+":"+person[1]+" PM";
+    } else if (person[0]==24){
+        person[2]=(person[0]-12)+":"+person[1]+" AM";
     } else {
-        if ((person[0])>13){
+        if ((person[0])>12){
             person[2]=(person[0]-12)+":"+person[1]+" PM";
         } else {
             person[2]=person[0]+":"+person[1]+" AM";
