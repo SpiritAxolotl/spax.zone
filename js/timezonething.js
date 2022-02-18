0350c90f2711af7f521010ba355de50ea449d5cc
SpiritAxolotl <dgnearman@gmail.com>
2022-02-17 18:34:08 -0700

    finally fixed js for timezone (hopefully last too)

--- js/timezonething.js
+++ js/timezonething.js
@@ -2,6 +2,7 @@ let d = new Date();
 let SpaxTime = [d.getUTCHours()-7,d.getUTCMinutes(),d.getUTCHours()+":"+d.getUTCMinutes()];
 let PentaTime = [d.getUTCHours()+7,d.getUTCMinutes(),d.getUTCHours()+":"+d.getUTCMinutes()];
 let SplitTime = [d.getUTCHours()-5,d.getUTCMinutes(),d.getUTCHours()+":"+d.getUTCMinutes()];
+let UTCTime = [d.getUTCHours(),d.getUTCMinutes(),d.getUTCHours()+":"+d.getUTCMinutes()];
 
 run();
 
@@ -22,8 +23,10 @@ function toTimezone(person){
     if (person[1]=="0"||person[1]=="1"||person[1]=="2"||person[1]=="3"||person[1]=="4"||person[1]=="5"||person[1]=="6"||person[1]=="7"||person[1]=="8"||person[1]=="9"){
         person[1]="0"+person[1]
     }
-    if ((person[0]<1)||(person[0]>24)){
-        person[0]=Math.abs(24-person[0])+1;
+    if (person[0]<1){
+        person[0]=24+person[0];
+    } else if (person[0]>24){
+        person[0]=24-person[0];
     }
     if (person[0]==12){
         person[2]=person[0]+":"+person[1]+" PM";
@@ -42,4 +45,5 @@ function updateText(){
 document.getElementById("date1").innerHTML = "Spax's current time is "+SpaxTime[2]+"! They're in MST.";
 document.getElementById("date2").innerHTML = "Pentaxel's current time is "+PentaTime[2]+"! He's in GMT+7.";
 document.getElementById("date3").innerHTML = "Splittikin's current time is "+SplitTime[2]+"! She's in EST.";
+//document.getElementById("date4").innerHTML = "UTC time is currently "+UTCTime[2]+".";
 }
