d9420f547a4b60386efaafaf3720699bcaccec3d
SpiritAxolotl <dgnearman@gmail.com>
2022-03-05 21:23:52 -0700

    Update scrollparallax.js

--- js/scrollparallax.js
+++ js/scrollparallax.js
@@ -1,10 +1,12 @@
 var bg = document.querySelector("#bg");
-
 document.body.onscroll = function() {
 
 document.getElementById('bg').style.backgroundPositionY = (window.scrollY / 2) + "px"
 }
 
-document.body.onscroll = function() {
-document.getElementById('bg').style.backgroundPositionX = (window.scrollY / 2) + "px"
+let pt = Date.time()
+let ct = Date.time()
+setInterval(xpos, 1)
+function xpos() {
+document.getElementById('bg').style.backgroundPositionX = (ct-pt) + "px"
 }
