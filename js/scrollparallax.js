d0efa9217cfaec0cf1c14976a916d157bd33af74
CalmBubbles <100333110+CalmBubbles@users.noreply.github.com>
2023-04-13 17:21:32 +0800

    Update scrollparallax.js

--- js/scrollparallax.js
+++ js/scrollparallax.js
@@ -3,10 +3,11 @@ document.body.onscroll = function() {
 
 document.getElementById('bg').style.backgroundPositionY = (window.scrollY / 2) + "px"
 }
-
+/*
 let pt = Date.time()
 let ct = Date.time()
 requestAnimationFrame(xpos, 1)
 function xpos() {
 document.getElementById('bg').style.backgroundPositionX = (ct-pt) + "px"
 }
+*/
