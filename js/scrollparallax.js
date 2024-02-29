7820224b128f3df5a97e56a762244bc5588268d4
Spax <gamecrusher218@gmail.com>
2024-02-28 21:01:12 -0700

    complete face overhaul

--- js/scrollparallax.js
+++ js/scrollparallax.js
@@ -1,13 +1,5 @@
 //thanks sora
-const bg = document.querySelector("#bg");
-document.body.onscroll = function() {
-  document.getElementById("bg").style.backgroundPositionY = (window.scrollY * 0.5) + "px";
+const body = document.getElementsByTagName("body")[0];
+document.onscroll = () => {
+  document.body.style.backgroundPositionY = (window.scrollY * 0.5) + "px";
 }
-/*
-let pt = Date.time();
-let ct = Date.time();
-requestAnimationFrame(xpos, 1);
-function xpos() {
-  document.getElementById("bg").style.backgroundPositionX = (ct-pt) + "px";
-}
-*/
