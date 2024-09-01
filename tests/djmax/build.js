bf8ee9a1735b7f831c101a5921f09cc860ebbc11
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-09-01 14:27:08 -0600

    djmax initial commit

---
+++ tests/djmax/build.js
@@ -0,0 +1,63 @@
+const XLSX = require("xlsx");
+const fs = require("fs");
+
+const readXLSXFile = (filePath) => {
+  const workbook = XLSX.readFile(filePath);
+  const data = {};
+  for (let i=1; i< workbook.SheetNames.length; i++) {
+    const sheetName = workbook.SheetNames[i];
+    const sheet = workbook.Sheets[sheetName];
+    const sheetData = [];
+    let colNames = [];
+    for (let c=1; sheet[`${numToLabel(c)}1`]; c++)
+      colNames.push(sheet[`${numToLabel(c)}1`].v);
+    let row = 2;
+    let col = 1;
+    let ref = sheet["!ref"].split(":").map(e=>{
+      const match = e.match(/([A-Z]+)(\d+)/);
+      return [labelToNum(match[1]), +match[2]];
+    });
+    while (row-2 < ref[1][1]) {
+      const rowData = {};
+      while (col <= ref[1][0]) {
+        const cell = numToLabel(col) + row;
+        if (sheet[cell]) {
+          rowData[colNames[col-1] ?? "Notes"] = sheet[cell].v;
+          if (col === 1 && sheet[cell].l)
+            rowData.Link = sheet[cell].l.Target;
+        }
+        col++;
+      }
+      col = 1;
+      if (Object.keys(rowData).length > 0)
+        sheetData.push(rowData);
+      row++;
+    }
+    //const sheetData = XLSX.utils.sheet_to_json(sheet);
+    data[sheetName] = sheetData;
+  };
+  return data;
+};
+
+const numToLabel = (n) => {
+  let columnLabel = "";
+  while (n > 0) {
+    n--;
+    columnLabel = String.fromCharCode(65 + (n % 26)) + columnLabel;
+    n = Math.floor(n / 26);
+  }
+  return columnLabel;
+};
+const labelToNum = (l) => {
+  let number = 0;
+  for (let i = 0; i < l.length; i++) {
+      number *= 26;
+      number += l.charCodeAt(i) - 64;
+  }
+  return number;
+};
+
+const filePath = "./tests/djmax/moD Jam MAX.xlsx";
+const xlsxData = readXLSXFile(filePath);
+
+fs.writeFileSync("./data/djmax.json", JSON.stringify(xlsxData));
---
+++ tests/djmax/script.js
@@ -0,0 +1,70 @@
+//const fs = require("fs");
+
+const fetchJsonData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok)
+      throw new Error(`HTTP error! status: ${response.status}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+let djmaxData = [];
+let allSongs = [];
+const main = async () => {
+  djmaxData = await fetchJsonData("/data/djmax.json");
+  Object.keys(djmaxData).forEach(game => {
+    djmaxData[game].forEach(song => {
+      song.Game = game;
+    });
+    allSongs.push(...djmaxData[game].filter(song => {
+      return song.Eligible === "✓";
+    }));
+  });
+  //console.log(allSongs.length);
+};
+
+main();
+
+const randomSong = () => {
+  const rand = allSongs[Math.floor(allSongs.length * Math.random())];
+  document.querySelector(`#title`).innerText = rand.Title;
+  document.querySelector(`#artist`).innerText = rand.Artist;
+  document.querySelector(`#game`).innerText = rand.Game;
+  if (rand.DLC) document.querySelector(`#game`).innerText += ` (${rand.DLC})`;
+  if (rand.Link) {
+    //cobaltFetch(rand.Link);
+    document.querySelector(`#title`).href = rand.Link;
+  }
+};
+
+const cobaltFetch = async (url) => {
+  const headers = new Headers();
+  headers.append("Accept", "application/json");
+  headers.append("Content-Type", "application/json");
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      url: url,
+      filenamePattern: "basic",
+      isAudioOnly: true,
+      aFormat: "ogg"
+    }),
+    headers: headers,
+  });
+  console.log(response);
+};
+
+/*const genYoutubeIframe = (id) => {
+  return `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+}
+
+const genSoundcloudIframe = (id) => {
+  return `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1149367978&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`
+}*/
+
+//console.log("fuck");
