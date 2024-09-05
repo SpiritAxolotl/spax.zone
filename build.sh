ee3ed6b49814555bddbb64b049c2dafc79caf2c0
Spax <imstuff@spax.zone>
2024-09-05 17:19:13 -0600

    dep.spax.zone is up so redirect to that and stop building it on here

--- build.sh
+++ build.sh
@@ -3,7 +3,3 @@ node ./js/buildDEPdialogue.js
 node ./tests/djmax/build.js
 mv ./html/* .
 mv ./tests/* .
-git clone --depth 1 https://github.com/SpiritAxolotl/Dwellers-Empty-Path-Plus
-mkdir ./DEP
-mv ./Dwellers-Empty-Path-Plus/www/* ./DEP
-rm -rf ./Dwellers-Empty-Path-Plus
