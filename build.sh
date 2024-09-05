eae14634b5ec4e214cbc5bd6d8564546c41c557b
Spax <imstuff@spax.zone>
2024-09-04 20:02:42 -0600

    fix with mkdir?

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ node ./tests/djmax/build.js
 mv ./html/* .
 mv ./tests/* .
 git clone --depth 1 https://github.com/SpiritAxolotl/Dwellers-Empty-Path-Plus
+mkdir ./DEP
 mv ./Dwellers-Empty-Path-Plus/www/* ./DEP
 rm -rf ./Dwellers-Empty-Path-Plus
