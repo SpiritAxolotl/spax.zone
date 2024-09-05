73fbce13a1f84a2ec4e5a4ac7760d48f139292c8
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-09-04 20:00:55 -0600

    gitignore update and DEP now exists at spax.zone/DEP

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ node ./js/buildDEPdialogue.js
 node ./tests/djmax/build.js
 mv ./html/* .
 mv ./tests/* .
+git clone --depth 1 https://github.com/SpiritAxolotl/Dwellers-Empty-Path-Plus
+mv ./Dwellers-Empty-Path-Plus/www/* ./DEP
+rm -rf ./Dwellers-Empty-Path-Plus
