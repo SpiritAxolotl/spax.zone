d9bddaad228cc21925d3839dbc0b0d8ebc4892dc
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2025-02-26 00:41:53 -0700

    updated build script (for scss)

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 npm install --silent
 sass scss/experimental.scss:css/experimental.css
+sass scss/depfont.scss:css/depfont.css
+sass scss/customfaces.scss:css/customfaces.css
+sass scss/depfaces.scss:css/depfaces.css
+sass scss/yugoslavia.scss:css/yugoslavia.css
 node ./js/addpagemetadata.js
 node ./js/buildDEPdialogue.js
 node ./js/js-controlled-webrings.js
