31d027efa8f44212217bd421211298da6b513d1b
Spax <imstuff@spax.zone>
2025-08-29 17:45:18 -0700

    insane horse refactor with better data object

--- tests/horse/horse-rotate.js
+++ tests/horse/horse-rotate.js
@@ -5,40 +5,34 @@ const { parseHTML } = require("linkedom");
 //progress represented by [0-1) so the general order is maintained when new sites are added
 let progress = 0;
 const scheduler = new ToadScheduler();
-const horseListProgressFile = `./data/horseListProgress.txt`;
-let horseList = ["every.horse"];
+const dataDirectory = `./data`;
+const horseProgressFile = `${dataDirectory}/horseProgress.txt`;
+const repoLink = "https://spax.zone/repo/tests/horse";
+const everyHorseLink = "https://every.horse/horse.json";
+const everyHorseBackupFile = `${dataDirectory}/every_horse.json`;
+/**
+ * WILL NOT HAVE .horse AFTER EVERY LINK
+ */
+let horseList = ["every"];
 
-const dataDirectory = `data`;
-const everyHorseFilepath = `${dataDirectory}/every_horse.json`;
-const actualHorseFilepath = `${dataDirectory}/actual_horse.json`;
-const registeredHorseFilepath = `${dataDirectory}/registered_horse.json`;
-const redirectsHorseFilepath = `${dataDirectory}/redirects_horse.json`;
-const blankHorseFilepath = `${dataDirectory}/blank_horse.json`;
-//const everyHorseDataFilepath = `${dataDirectory}/every_horse_data.json`;
+const horseDataFilepath = `${dataDirectory}/horse_data.json`;
 
-const horseBins = {
-  registered: new Set(), //as in JUST registered and no ip
-  blank: new Set(),
-  parked: new Set(),
-  redirects: new Set(),
-  actual: new Set()
-};
-//let everyHorseData = {};
-
-const horseFilepath = (horseType) => {
-  return `data/${horseType}_horse.json`;
-};
+let horseData = {};
+let sortedHorseDataKeys = [];
+const defaultHorseDomainData = { status: "unvisited" };
 
 const createAnsiHyperlink = (url, text) => {
   return `\u001b]8;;${url}\u001b\\${text}\u001b]8;;\u001b\\`;
 };
 
 const getHTMLFile = async (link, local, ua) => {
+  let userAgent = repoLink;
+  if (ua) userAgent = `${ua} (${repoLink})`;
   try {
     const headers = new Headers({
       "Accept": "text/html",
       "Content-Type": "text/html",
-      "User-Agent": `${ua?ua + " (":""}https://spax.zone/repo/tests/horse${ua?")":""}`
+      "User-Agent": userAgent
     });
     
     const response = await fetch(link, {
@@ -67,7 +61,7 @@ const getJSONFile = async (link) => {
     const headers = new Headers({
       "Accept": "application/json",
       "Content-Type": "application/json",
-      "User-Agent": "Spax's Daily Horselist Fetch (https://spax.zone/repo/tests/horse)"
+      "User-Agent": `Spax's Daily Horselist Fetch (${repoLink})`
     });
     
     const response = await fetch(link, {
@@ -84,8 +78,8 @@ const getJSONFile = async (link) => {
 //for transparency, this method was written by claude.ai, based off of my old code which wasn't as good
 //i edited out the comments and tweaked it to my own preferences
 //my old code can be found in this gist: https://gist.github.com/SpiritAxolotl/a8bbe4dea39eedb4a750c66702a44a7f
-const sortHorseList = (list) => {
-  list.sort((a, b) => {
+const sortHorseList = (list=[]) => {
+  return list.toSorted((a, b) => {
     const aMatch = a.match(/((?:[\w_-]+\.)*)((?:[\w_-]+\.)[\w_-]+)/);
     const bMatch = b.match(/((?:[\w_-]+\.)*)((?:[\w_-]+\.)[\w_-]+)/);
     
@@ -129,138 +123,136 @@ const sortHorseList = (list) => {
     }
     return a.localeCompare(b);
   });
-  
-  return list;
 };
 
-const incrementProgress = async () => {
-  const p = Math.round(progress * horseList.length);
-  progress = ((p+1)%horseList.length)/horseList.length;
-  fs.writeFileSync(horseListProgressFile, `${progress}`);
+const incrementProgress = () => {
+  const len = sortedHorseDataKeys.length;
+  let p = Math.round(progress * len);
+  while (sortedHorseDataKeys[p].status === "dead" && !horseList.includes(sortedHorseDataKeys[p]))
+    p++; //skip dead horses
+  progress = ((p+1)%len)/len;
+  fs.writeFileSync(horseProgressFile, `${progress}`);
   return p;
 };
 
-const binAdjust = (domain, ...arr) => {
-  const bins = ["blank", "parked", "registered", "redirects", "actual"];
-  for (const bin of bins) {
-    if (arr.includes(bin)) {
-      horseBins[bin].add(domain);
-    } else {
-      horseBins[bin].delete(domain);
-    }
-  }
-  fs.writeFileSync(`./${actualHorseFilepath}`, JSON.stringify(sortHorseList([...horseBins.actual])));
-  fs.writeFileSync(`./${registeredHorseFilepath}`, JSON.stringify(sortHorseList([...horseBins.registered])));
-  fs.writeFileSync(`./${redirectsHorseFilepath}`, JSON.stringify(sortHorseList([...horseBins.redirects])));
-  fs.writeFileSync(`./${blankHorseFilepath}`, JSON.stringify(sortHorseList([...horseBins.blank])));
-};
-
-const parseHorseListSite = (document) => {
+/*const parseHorseListSite = (document) => {
   let list = [];
   const stats = document.querySelector(`main.page-content div.wrapper div.home p:nth-of-type(2)`).textContent
     .match(/There are currently (\d+) horse domains. \(Last checked at: (\d{1,2} [A-Z][a-z]*, \d{1,})\)/);
   list = Array.from(document.querySelectorAll(`#the-list + p > a`))
-    .map(a => a.textContent.match(/[\w_-]+(?:\.[\w_-]+)*/)[0]);
+    .map(a => a.textContent.match(/[\w_-]+(?:\.[\w_-]+){0,}/)[0]);
   list.splice(list.findIndex(e=>e==="horse"), 1);
   return list;
+};*/
+
+const getHorseFromList = (n) => {
+  return `${horseList[n]}.horse`;
 };
 
 const fetchHorseList = async () => {
   console.log("Fetching horselist from every.horse/horse.json...");
+  const horseDomainRegex = /((?:[\w_-]+\.)*[\w_-]+)(?=\.horse)/;
+  let pureHorseList = [...horseList];
   try {
-    horseList = JSON.parse(await getHTMLFile("https://every.horse/horse.json", undefined, "Spax's Periodic Horse Domain Check-Up"));
-    //const { document } = parseHTML(data);
-    //horseList = parseHorseListSite(document);
-    sortHorseList(horseList);
+    pureHorseList = await getJSONFile(everyHorseLink);
+    fs.writeFileSync(everyHorseBackupFile, JSON.stringify(sortHorseList(pureHorseList)));
     console.log("Horselist retreived!\n");
-    fs.writeFileSync(`./${everyHorseFilepath}`, JSON.stringify(horseList));
-    return;
   } catch (err) {
     console.log("Unable to get new horselist. Here's the error:");
     console.error(err);
-    /*
-    console.log("Attempting to use an archived list...");
+    if (!fs.existsSync(everyHorseBackupFile)) {
+      console.log("Attempting to use backup file...");
+      horseList = JSON.parse(fs.readFileSync(everyHorseBackupFile));
+      console.log("Backup file used!\n");
+    } else return false;
   }
-  try {
-    const data = await getHTMLFile(`https://web.archive.org/web/${(new Date()).getFullYear() + 1}0000000000/https://every.horse/horse.json`, undefined, "Spax's Periodic Horse Domain Check-Up");
-    const { document } = parseHTML(data);
-    horseList = parseHorseListSite(document);
-    sortHorseList(horseList);
-    console.log("Horselist retreived!\n");
-    fs.writeFileSync(`./${everyHorseFilepath}`, JSON.stringify(horseList));
-    return;
-  } catch {
-    console.log("Unable to get archived horselist. Here's the error:");
-    console.error(err);
-    //console.log("Attempting to use an old list...");
-    //horseList = getJSONFile(`https://spax.zone/${filepath}`);
-    */
-  }
+  horseList = pureHorseList
+    .filter(e=>e.match(horseDomainRegex)) //filters to those that will match the next regex
+    .map(e=>e.match(horseDomainRegex)[0]); //removes the .horse
+  horseList = sortHorseList(horseList);
+  for (const horseSLD of horseList)
+    horseData[horseSLD] ??= {...defaultHorseDomainData};
+  sortedHorseDataKeys = sortHorseList(Object.keys(horseData));
+  fs.writeFileSync(`./${horseDataFilepath}`, JSON.stringify(horseData));
+  return true;
 };
 
-const horseRotate = async (params={domain:undefined}) => {
+const horseRotate = async (params={sld:""}) => {
   let p = 0;
-  const debugDomain = typeof params.domain === "string";
-  if (!debugDomain) {
-    console.log(`Current progress: ${(progress * 100).toFixed(3)}%`);
-    p = await incrementProgress();
+  
+  const horseUpdate = (status="", updates={}) => {
+    horseData[horseList[p]] ??= {};
+    const targetHorse = horseData[horseList[p]];
+    targetHorse.status = status;
+    delete targetHorse.redirect;
+    let consoleStatus = status.substring(0,1).toUpperCase() + status.substring(1);
+    switch (status) {
+      case "redirect":
+        targetHorse.redirect = updates.redirect;
+      case "registered":
+        consoleStatus += " but IP-less";
+      case "dead":
+        consoleStatus += " 💀";
+        break;
+      default:
+        targetHorse.lastVisited = new Date();
+    }
+    
+    fs.writeFileSync(`./${horseDataFilepath}`, JSON.stringify(horseData));
+    console.log(`Verdict: ${consoleStatus}\n`);
+  };
+  
+  const specifiedDomain = typeof params.sld === "string" && params.sld.length > 0;
+  if (!specifiedDomain) {
+    console.log(`Current progress: ${(progress * 100).toFixed(4)}%`);
+    p = incrementProgress();
   }
-  const horse = params.domain ?? horseList[p];
+  const horse = specifiedDomain ? `${params.sld}.horse` : getHorseFromList(p);
   console.log(`Current date and time: ${(new Date()).toString()}`);
   console.log("Current horse domain:", /*createAnsiHyperlink(`http://${horse}`, horse),*/ horse);
+  
   let document = undefined;
   let fetchResponse = new Response();
   try {
     const headers = new Headers({
       "Accept": "text/html",
       "Content-Type": "text/html",
-      "User-Agent": `Spax's Periodic Horse Domain Check-Up (https://spax.zone/repo/tests/horse)`
+      "User-Agent": `Spax's Periodic Horse Domain Check-Up (${repoLink})`
     });
     
-    try {
-      fetch(`http://${horse}`, {
-        method: "GET",
-        headers: headers
-      }).then((response) => {
-        fetchResponse = response;
-        const regex = new RegExp(`^https?:\\/\\/(?:[\\w_-]+\\.)*${horse.replaceAll(".", "\\.")}(?:$|\\/)`);
-        if (response.redirected && response.url.match(regex) === null) {
-          binAdjust(horse, "redirects");
-          console.log("Verdict: Redirect\n");
-          return;
-        } else if (response.ok) {
-          return response.text();
+    await fetch(`http://${horse}`, {
+      method: "GET",
+      headers: headers
+    }).then((response) => {
+      fetchResponse = response;
+      const regex = new RegExp(`^https?:\\/\\/(?:[\\w_-]+\\.)*${horse.replaceAll(".", "\\.")}(?:$|\\/)`);
+      if (response.redirected && response.url.match(regex) === null) {
+        horseUpdate({status:"redirect", redirect: response.url});
+        return;
+      } else if (response.ok) {
+        return response.text();
+      } else {
+        horseUpdate("registered");
+        return;
+      }
+    }).then((text) => {
+      if (text && text !== "") {
+        document = parseHTML(text);
+        const url = new URL(fetchResponse.url);
+        if (text.includes(`<script>window.onload=function(){window.location.href="/lander"}</script>`)
+        || (url.pathname === "/lander" && document.querySelector(`body > div#root:empty`))) { //blank
+          horseUpdate("blank");
         } else {
-          binAdjust(horse, "registered");
-          console.log("Verdict: Registered but IP-less\n");
-          return;
+          horseUpdate("actual");
         }
-      }).then((text) => {
-        if (text && text !== "") {
-          document = parseHTML(text);
-          const url = new URL(fetchResponse.url);
-          if (text.includes(`<script>window.onload=function(){window.location.href="/lander"}</script>`)
-          || (url.pathname === "/lander" && document.querySelector(`body > div#root:empty`))) { //blank
-            binAdjust(horse, "blank");
-            console.log("Verdict: Blank\n");
-          } else {
-            binAdjust(horse, "actual");
-            console.log("Verdict: Actual\n");
-          }
-          return;
-        } else if (text === "") {
-          binAdjust(horse, "blank");
-          console.log("Verdict: Blank\n");
-          return;
-        }
-      }).catch((e) => {
-        binAdjust(horse, "registered");
-        console.log("Verdict: Registered but IP-less\n");
-      });
-    } catch (e) {
-      binAdjust(horse, "registered");
-      console.log("Verdict: Registered but IP-less\n");
-    }
+        return;
+      } else if (text === "") {
+        horseUpdate("blank");
+        return;
+      }
+    }).catch((e) => {
+      horseUpdate("registered");
+    });
   } catch (err) {
     console.error(err);
   }
@@ -271,44 +263,50 @@ const tasks = {
   horseRotate: new Task("horse rotate", horseRotate)
 };
 const jobs = {
-  fetchHorseList: new SimpleIntervalJob({ seconds: 60 * 60 * 24, }, tasks.fetchHorseList), //24h
-  horseRotate: new SimpleIntervalJob({ seconds: 60 * 5, }, tasks.horseRotate) //5m
+  fetchHorseList: new SimpleIntervalJob({ days: 1 }, tasks.fetchHorseList),
+  horseRotate: new SimpleIntervalJob({ minutes: 5, runImmediately: true }, tasks.horseRotate)
 };
 
 const restoreProgress = () => {
-  if (!fs.existsSync(horseListProgressFile))
-    fs.writeFileSync(horseListProgressFile, `${progress}`);
-  else
-    progress = +fs.readFileSync(horseListProgressFile);
+  if (!fs.existsSync(horseProgressFile))
+    fs.writeFileSync(horseProgressFile, `${progress}`);
+  else {
+    const progressText = fs.readFileSync(horseProgressFile);
+    if (progressText.includes("new")) {
+      return true;
+    } else if (typeof (+progressText) === "number" && !isNaN(+progressText)) {
+      progress = +progressText;
+    }
+  }
   
-  if (fs.existsSync(actualHorseFilepath))
-    horseBins.actual = new Set(JSON.parse(fs.readFileSync(actualHorseFilepath)));
-  if (fs.existsSync(registeredHorseFilepath))
-    horseBins.registered = new Set(JSON.parse(fs.readFileSync(registeredHorseFilepath)));
-  if (fs.existsSync(redirectsHorseFilepath))
-    horseBins.redirects = new Set(JSON.parse(fs.readFileSync(redirectsHorseFilepath)));
-  if (fs.existsSync(blankHorseFilepath))
-    horseBins.blank = new Set(JSON.parse(fs.readFileSync(blankHorseFilepath)));
+  if (fs.existsSync(horseDataFilepath))
+    horseData = JSON.parse(fs.readFileSync(horseDataFilepath));
+  return false;
+};
+
+const getUnvisitedHorses = () => {
+  return sortedHorseDataKeys.filter(horse=>horseData[horse].status === "unvisited");
 };
 
 const main = async () => {
-  restoreProgress();
-  await fetchHorseList();
-  for (const job in jobs)
-    scheduler.addSimpleIntervalJob(jobs[job]);
-  await horseRotate();
+  let firstRun = restoreProgress();
+  const success = await fetchHorseList();
+  const unvisitedHorses = getUnvisitedHorses();
+  if (success) {
+    if (firstRun || unvisitedHorses.length > 0) {
+      console.log("First run detected! Removing the rotation cooldown...\n");
+      while (unvisitedHorses.length > 0) {
+        await horseRotate(unvisitedHorses.shift());
+      }
+      console.log("Unvisited sites have all been visited! Starting normal rotation...\n");
+    }
+    for (const job in jobs) {
+      scheduler.addSimpleIntervalJob(jobs[job]);
+    }
+  } else {
+    console.log("Unable to fetch the horse list. Terminating program...");
+  }
 };
 
 if (require.main === module)
   main();
-
-/*
-fetch horselist from every.horse IF /data/every_horse.json is empty or doesn't exist
-start every.horse daily pull
-  write to /data/every_horse.json
-start horselist rotate pull
-  go to .horse website based on index
-  determine it to be one of five things
-  put the domain into categorical json files based on what it was determined to be
-
-*/
