6b9047bcb7d3a7e9eb3315e846ce1c7a25a701d9
Spax <imstuff@spax.zone>
2026-06-02 21:45:48 -0600

    more parked domain detection techniques

--- tests/horse/parked-domain-detection.js
+++ tests/horse/parked-domain-detection.js
@@ -4,18 +4,35 @@ const defaultHTML = "<!DOCTYPE HTML><html><head></head><body></body></html>";
 
 const detectParkedDomain = (document=parseHTML(defaultHTML).document, sld) => {
   const domain = `${sld}.horse`;
+  const icons = [
+    "//img.sedoparking.com/templates/logos/sedo_logo.png",
+    "/__ovh/common/img/favicon.ico",
+    "https://custom.rebrandly.com/img/rb_favicon_rounded.ico"
+  ]
   const tests = [
-    _ => document?.title === `${domain} - ${sld} Resources and Information.`,
-    _ => document?.title === `${domain} is coming soon`,
-    _ => document?.title === `porkbun.com | parked domain`,
+    _ => document?.title?.includes(`${sld} Resources and Information.`),
+    _ => document?.title?.includes(`${sld}.horse is coming soon`),
+    _ => document?.title?.match(/porkbun\.com \| parked domain/i),
     _ => document.querySelector(`#plBanner > img[alt="Namecheap banner"]`) !== null,
     _ => document.querySelector(`#dn-default > h1`)?.textContent === domain,
-    _ => (document?.head?.outerHTML + document?.body?.outerHTML).match(/parked/ig)?.length >= 2,
+    _ => icons.some(icon=>document?.querySelector(`link[rel~="icon"][href]`)?.href?.includes(icon)),
+    _ => (document.head?.outerHTML + document?.body?.outerHTML)?.match(/parked/ig)?.length >= 2,
+    _ => Array.from(document?.querySelectorAll(`h1`)).some(h1=>h1.textContent.match(/(construction|registered|launching soon)/i)),
+    _ => document.querySelector(`[id^="_ol_"]`),
+    _ => document.querySelector(`img[src="//assets.squarespace.com/universal/images-v6/parking-page/backgrounds/img101-landscape.jpg"]`),
+    _ => document.querySelector(`p[style="width: 100%; margin: auto; display: table; text-align: center; vertical-align: middle; font: 400 16px/90vh 'Proxima Nova', 'Lato', sans-serif;"]`),
+    _ => document.querySelector(`meta[content="NOW"][name="expires"]`),
+    _ => getComputedStyle(document.body).backgroundImage.match(/Parking\.jpg/i),
+    _ => document.querySelector(`#GODADDY_FREEMIUM_AD`)
   ];
   return tests.some(test => {
     if (document.head && document.body) {
       try {
-        return test();
+        const result = test()
+        if (result) {
+          console.log(`Passed parked domain test ${tests.indexOf(test) + 1}!`);
+        }
+        return result;
       } catch (error) {
         if (error)
         console.log(`Error on parked domain test ${tests.indexOf(test) + 1}:\n`, error.name + ":", error.message);
