6065bd284b11339cff8b456263e7ec0e8b26dfec
Spax <imstuff@spax.zone>
2026-07-25 02:25:28 -0600

    this shit is so ass. fix build.sh hopefully

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # a better script will be written soon
-sass scss/experimental.scss:css/experimental.css --silence-deprecation=mixed-decls
-sass scss/depfont.scss:css/depfont.css --silence-deprecation=mixed-decls
-sass scss/customfaces.scss:css/customfaces.css --silence-deprecation=mixed-decls
-sass scss/depfaces.scss:css/depfaces.css --silence-deprecation=mixed-decls
-sass scss/yugoslavia.scss:css/yugoslavia.css --silence-deprecation=mixed-decls
+sass scss/game.scss:css/game.css
+sass scss/depfont.scss:css/depfont.css
+sass scss/customfaces.scss:css/customfaces.css
+sass scss/depfaces.scss:css/depfaces.css
+# sass scss/yugoslavia.scss:css/yugoslavia.css
 node ./scripts/genericPageScripts.js
 node ./scripts/buildDEPdialogue.js
-# node ./scripts/js-controlled-webrings.js
+node ./scripts/js-controlled-webrings.js
 mv ./html/* .
 mv ./tests/* .
