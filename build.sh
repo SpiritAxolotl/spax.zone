5212a732ae3c727520e01df11a3a12f9ddbcbbf4
Spax <imstuff@spax.zone>
2026-07-25 02:50:39 -0600

    build.sh redo

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 # a better script will be written soon
+rm -rf .build
+mkdir .build
+rsync -a . .build --exclude .build --exclude=".*" --exclude=node_modules
+cd .build
+
+mv ./html/* .
+mv ./tests/* .
 sass scss/game.scss:css/game.css
 sass scss/depfont.scss:css/depfont.css
 sass scss/customfaces.scss:css/customfaces.css
 sass scss/depfaces.scss:css/depfaces.css
 # sass scss/yugoslavia.scss:css/yugoslavia.css
-node ./scripts/genericPageScripts.js
-node ./scripts/buildDEPdialogue.js
-node ./scripts/js-controlled-webrings.js
-mv ./html/* .
-mv ./tests/* .
+node ../scripts/genericPageScripts.js
+node ../scripts/buildDEPdialogue.js
+node ../scripts/js-controlled-webrings.js
+
+cd ..
